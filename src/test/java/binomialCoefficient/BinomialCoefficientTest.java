763334cfc6f442c532404d2a05c160a5cbcf21e8
Nihar Parikh <nihar17999@gmail.com>
2019-11-01 06:15:24 -0400

    Created a successful test for factorial

--- src/test/java/binomialCoefficient/BinomialCoefficientTest.java
+++ src/test/java/binomialCoefficient/BinomialCoefficientTest.java
@@ -1,4 +1,13 @@
 package binomialCoefficient;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BinomialCoefficientTest {
+    @Test
+    public void factorialTest10_3628800() {
+        long expected = 3628800;
+        long actual = BinomialCoefficient.factorial(10);
+        assertEquals(expected, actual);
+    }
 }
