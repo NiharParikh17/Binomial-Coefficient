579185b47887e2d0ba3a59105e05c9815422cd2c
Nihar Parikh <nihar17999@gmail.com>
2019-11-01 06:31:35 -0400

    Created method for pascals identity

--- src/main/java/binomialCoefficient/BinomialCoefficient.java
+++ src/main/java/binomialCoefficient/BinomialCoefficient.java
@@ -14,4 +14,13 @@ public class BinomialCoefficient {
         long denominator = factorial(k)*factorial(n-k);
         return numerator/denominator;
     }
+
+    public static long pascalsMethod(int n, int k) {
+        if (k>n || k<0) // Invalid input case
+            return -1;
+
+        if (k==n || k==0) // Base Case: k choose k OR n choose 0
+            return 1;
+        return pascalsMethod(n-1, k-1) + pascalsMethod(n-1, k);
+    }
 }
