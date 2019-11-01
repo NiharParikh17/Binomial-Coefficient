e010baeaccd7c9fc0c62d38c4665dada02d08a97
Nihar Parikh <nihar17999@gmail.com>
2019-11-01 06:21:06 -0400

    Created definition method

--- src/main/java/binomialCoefficient/BinomialCoefficient.java
+++ src/main/java/binomialCoefficient/BinomialCoefficient.java
@@ -6,4 +6,12 @@ public class BinomialCoefficient {
             return 1;
         return n*factorial(n-1);
     }
+
+    public static long definitionMethod(int n, int k) {
+        if (k>n || k<0) // Invalid input case
+            return -1;
+        long numerator = factorial(n);
+        long denominator = factorial(k)*factorial(n-k);
+        return numerator/denominator;
+    }
 }
